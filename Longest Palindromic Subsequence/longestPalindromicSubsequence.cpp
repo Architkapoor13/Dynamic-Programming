54b86222040c8f1cdca0ca8bfdb262cfa1dd8e1c
Architkapoor13 <architkapoor13@gmail.com>
2022-02-09 07:38:03 +0530

    questions on palindrome!

---
+++ Longest Palindromic Subsequence/longestPalindromicSubsequence.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int LCS(string a, int m, string b, int n){
+        int **t = new int*[m+1];
+        for(int i = 0; i < m + 1; i++){
+            t[i] = new int [n+1];
+        }
+        for(int i = 0; i < m + 1; i++){
+            t[i][0] = 0;
+        }
+        for(int i = 0; i < n + 1; i++){
+            t[0][i] = 0;
+        }
+        for(int i = 1; i < m + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(a[i-1] == b[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j], t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
+    }
+    
+    int longestPalinSubseq(string A) {
+        string rev = "";
+        for(int i = A.size()-1; i >= 0; i--){
+            rev += A[i];
+        }
+        return LCS(A, A.size(), rev, rev.size());
+    }
+
+int main(){
+    string a;
+    cin >> a;
+    cout << longestPalinSubseq(a) << endl;
+    return 0;
+    
+}
